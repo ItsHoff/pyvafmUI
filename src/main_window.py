d7a82f00cec7508029426459a742fa1583d49284
ItsHoff <ItsHoff@users.noreply.github.com>
2014-08-29 11:50:37 +0300

    Added window icon.

--- src/main_window.py
+++ src/main_window.py
@@ -33,12 +33,14 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         """Initialise the main window."""
         super(MainWindow, self).__init__()
+        self.setWindowIcon(QtGui.QIcon("../images/logo.jpg"))
+        self.setWindowIconText("PyVAFM UI")
         self.initUI()
 
     def initUI(self):
         """Initialise the UI elements of the main window."""
         self.setGeometry(100, 100, 800, 500)
-        self.setWindowTitle('PyVAFM')
+        self.setWindowTitle('PyVAFM UI')
 
         file_menu = QtGui.QMenu("File", self)
 
